3d86d4f816c31cc3af69a6acf9e532c4a290b0c7
Kernel114514 <Kernel114514@hotmail.com>
2025-11-29 22:45:34 +0800

    Use proper Android makefile structure

--- XiaomiParts.mk
+++ XiaomiParts.mk
@@ -1,3 +1,1 @@
-$(call inherit-product, vendor/xiaomi/XiaomiParts/frameworks/frameworks.mk)
-$(call inherit-product, vendor/xiaomi/XiaomiParts/framework-apps/framework-apps.mk)
-$(call inherit-product, vendor/xiaomi/XiaomiParts/nonsystem-apps/nonsystem-apps.mk)
+$(call all-makefiles-under, $(call my-dir))
